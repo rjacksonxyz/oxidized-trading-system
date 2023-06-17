cd10b4ff5d32c23a28afbd0b6c0ca32907a491c0
Robert Jackson <jackson.t.robert@gmail.com>
2023-06-17 15:25:31 -0500

    chore(): adding code for  data retrieval, replacement of  client with  request (due to tokio runtime compatibility issues with .

--- oxidized/src/bin/main.rs
+++ oxidized/src/bin/main.rs
@@ -2,15 +2,12 @@ use oxidized::oxidized::utils::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let df = get_tickers_info().await;
-    match df {
+    let df = get_sp500_df().await;
+        match df {
         Ok(info) => {
-            println!("{:?}", info); 
-            return Ok(())
+            println!("{:?}", info);
+            return Ok(());
         }
-        Err(e) => {
-            return Err(e)
-        }
+        Err(e) => return Err(e),
     }
 }
-
--- oxidized/src/oxidized/utils.rs
+++ oxidized/src/oxidized/utils.rs
@@ -1,22 +1,19 @@
 use polars::prelude::*;
-use reqwest::Client;
 use scraper::{Html, Selector};
+use std::error::Error;
+use yahoo_finance::history;
 
-pub async fn get_tickers_info() -> Result<DataFrame, Box<dyn std::error::Error>> {
-    // Create an async client
-    let client = Client::builder().build()?;
-    let response = client
-        .get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-        .send()
-        .await?
-        .text()
-        .await?;
+pub async fn get_tickers_info() -> Result<DataFrame, Box<dyn Error>> {
 
-    let document = Html::parse_document(&response);
+    //submit a simple HTTP request
+    let response = ureq::get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies").call();
+    let response_str = response.unwrap().into_string()?;
+
+    let document = Html::parse_document(response_str.as_str());
     let table_selector = Selector::parse("table").unwrap();
     let mut columns = vec![];
     let mut values = vec![];
-    
+
     // Iterate over html table
     if let Some(table) = document.select(&table_selector).next() {
         for (i, row) in table.select(&Selector::parse("tr").unwrap()).enumerate() {
@@ -37,13 +34,12 @@ pub async fn get_tickers_info() -> Result<DataFrame, Box<dyn std::error::Error>>
     let row_len = values[0].len();
     let mut step = 0;
     while step < row_len {
-        let column_values: Vec<String> =
-            values.iter().map(|row| row[step].clone()).collect();
+        let column_values: Vec<String> = values.iter().map(|row| row[step].clone()).collect();
         let col_series = Series::new(&columns[step], column_values);
         series.push(col_series);
         step += 1;
     }
-    
+
     // Create new DataFrame
     let df = DataFrame::new_no_checks(series);
     Ok(df)
@@ -57,4 +53,31 @@ fn extract_link_text(input: String) -> Option<String> {
     } else {
         Some(input)
     }
+}
+
+/*
+    internally, yahoo_finance returns data in the form of Vec<Bar>
+    Bar {
+        timestamp: 1674484200000,
+        open: 59.29999923706055,
+        high: 60.209999084472656,
+        low: 58.9900016784668,
+        close: 60.209999084472656,
+        volume: Some(
+            1072300,
+        ),
+    },
+ */
+// TODO: parallelize the process of Vec<Bar> data, obscenely slow otherwise.
+pub async fn get_sp500_df() -> Result<DataFrame, Box<dyn Error>> {
+    let df = get_tickers_info().await?;
+    let symbol_series = df.column("Symbol").unwrap().utf8()?;
+    for symbol in symbol_series.into_iter() {
+        if let Some(sym) = symbol {
+            println!("getting data for {}", sym);
+            let data = history::retrieve_interval(sym, yahoo_finance::Interval::_10y).await?;
+            dbg!(data);
+        }
+    }
+    Ok(df)
 }
